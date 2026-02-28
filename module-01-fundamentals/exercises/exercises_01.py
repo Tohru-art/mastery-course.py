607006bd685a1adc822c3ea6b38e34e5f703b502
Will-Garlens Pierre <willgarlensp@gmail.com>
2026-02-28 03:41:14 -0500

    feat(module-1): working on exercise set 1

--- module-01-fundamentals/exercises/exercises_01.py
+++ module-01-fundamentals/exercises/exercises_01.py
@@ -16,8 +16,6 @@ When you finish, commit:
 def celsius_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit. Formula: (C * 9/5) + 32"""
     # YOUR CODE HERE
-    # This line is just a test
-    # Yest it changed
     pass
 
 
